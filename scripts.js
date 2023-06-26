1e739b4d39d134723146657f74d441a30f408b52
Alex Hutton <amhutton@email.sc.edu>
2023-06-26 11:05:20 -0400

    Added Name Slot Machine

--- scripts.js
+++ scripts.js
@@ -9,3 +9,38 @@ function formatPhoneNumber(number) {
     const formattedNumber = number.replace(/^(\d{3})(\d{3})(\d{4})$/, '$1-$2-$3');
     return formattedNumber;
 }
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const letterElement = document.getElementById('letter');
+const selectButton = document.querySelector('.select-button');
+const stopButton = document.querySelector('.stop-button');
+const selectedLettersContainer = document.querySelector('.selected-letters');
+const slotMachine = document.querySelector('.slot-machine');
+let currentIndex = 0;
+let intervalId;
+
+function spinSlotMachine() {
+    intervalId = setInterval(() => {
+        letterElement.textContent = letters[currentIndex];
+        currentIndex = (currentIndex + 1) % letters.length;
+    }, 100);
+}
+
+function selectLetter() {
+    const selectedLetter = letters[currentIndex];
+    const selectedLetterElement = document.createElement('span');
+    selectedLetterElement.textContent = selectedLetter;
+    selectedLettersContainer.appendChild(selectedLetterElement);
+}
+
+function stopSlotMachine() {
+    clearInterval(intervalId);
+    slotMachine.style.display = 'none';
+    stopButton.style.display = 'none';
+    selectButton.style.display = 'none';
+
+}
+
+selectButton.addEventListener('click', selectLetter);
+stopButton.addEventListener('click', stopSlotMachine);
+spinSlotMachine();
